52a4ca88660cf008d1cd65d497f9e829cc2b035e
damienbod <damien_bod@hotmail.com>
2019-09-17 21:07:08 +0200

    updating nuget packages

--- AspNetCoreWebApiContribProtobufSample/Startup.cs
+++ AspNetCoreWebApiContribProtobufSample/Startup.cs
@@ -19,11 +19,6 @@ namespace AspNetCoreWebApiContribProtobufSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<IISServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
-
             services.AddControllers()
                 .AddProtobufFormatters();
         }
